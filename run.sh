aa4cc9ac5db23834be4d9038248d7dff6c343742
N O <no140@columbia.edu>
2016-07-11 17:18:41 -0400

    new py file and related updates

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@
 
 # I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
 
-python3 ./src/rolling_median_noplot.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
+python3 ./src/rolling_median_noplot_opt.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
+#python3 ./src/rolling_median_noplot.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 #python3 ./src/rolling_median_netwk_noplot.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 #python3 ./src/rolling_median_netwk.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 
